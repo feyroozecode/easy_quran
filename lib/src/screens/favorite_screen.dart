8a41b5a4edd51245cb7689edbfaf3b4f215ea1e1
Ibrahim Ahmad (feyroozecode) <43380715+feyroozecode@users.noreply.github.com>
2023-12-09 14:03:24 +0100

    Create favorite_screen.dart

---
+++ lib/src/screens/favorite_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+public class FavoriteScreen extends Statefulwidget {
+
+  createState() =>  
+}
+
+public class FavoriteScreenState extends State<FavoriteScreen> {
+
+  @overrride
+  build(BuildContext context) {
+    return Center(
+      child: Text('Favorite Screen')
+    );
+  }
+}
